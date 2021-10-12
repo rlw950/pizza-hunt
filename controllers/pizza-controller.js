3b387bf8519779b7f5f0615ff578821ab6bd5e0e
Ray Computer <rlw950@yahoo.com>
2021-10-12 12:31:55 -0700

    feature/validation

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -37,7 +37,7 @@ const pizzaController = {
                 res.status(400).json(err);
             });
     },
-    
+
     // createPizza
     createPizza({ body }, res) {
         Pizza.create(body)
@@ -47,7 +47,7 @@ const pizzaController = {
 
     // update pizza by id
     updatePizza({ params, body }, res) {
-        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id!' });
